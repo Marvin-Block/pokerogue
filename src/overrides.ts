5aa2c6247bf817ada3ba3d073d67eff09291c4a7
Marvin Block <marvin.block1998@gmail.com>
2024-05-26 18:34:48 +0200

    weekend special double xp bonus

--- src/overrides.ts
+++ src/overrides.ts
@@ -31,7 +31,7 @@ export const STARTING_WAVE_OVERRIDE: integer = 0;
 export const STARTING_BIOME_OVERRIDE: Biome = Biome.TOWN;
 export const ARENA_TINT_OVERRIDE: TimeOfDay = null;
 // Multiplies XP gained by this value including 0. Set to null to ignore the override
-export const XP_MULTIPLIER_OVERRIDE: number = null;
+export const XP_MULTIPLIER_OVERRIDE: number = 2;
 // default 1000
 export const STARTING_MONEY_OVERRIDE: integer = 0;
 export const POKEBALL_OVERRIDE: { active: boolean, pokeballs: PokeballCounts } = {
